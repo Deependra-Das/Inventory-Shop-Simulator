23feead86ea678544f4dc1f084327a0c9c766131
Deependra-Das <iamdeep75@gmail.com>
2025-02-05 14:29:31 +0530

    Added Skeleton Files

---
+++ Assets/Scripts/Main/GameService.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ServiceLocator.Main
+{
+    public class GameService : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
