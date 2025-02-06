ad9c85d71ead474b5b211f4e5a5c51cbd69edd16
Deependra-Das <iamdeep75@gmail.com>
2025-02-06 17:57:09 +0530

    Changes to populate Inventory with SO

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -1,7 +1,31 @@
+using UnityEngine;
+using System.Collections.Generic;
+using ServiceLocator.Item;
+
 namespace ServiceLocator.Inventory
 {
     public class InventoryController
     {
+        private InventoryScriptableObject _inventoryCurrentData;
+        private List<ItemController> itemControllers;
 
+        public InventoryController(InventoryScriptableObject inventoryCurrentData)
+        {
+            this._inventoryCurrentData = inventoryCurrentData;
+            itemControllers = new List<ItemController>();
+        }
+
+        ~InventoryController()
+        {
+            this._inventoryCurrentData.inventoryItemList = new List<ItemScriptableObject>();
+        }
+        public void AddNewItemInInventory(ItemScriptableObject itemData, ItemService itemService)
+        {
+            ItemController itemController = itemService.CreateItem(itemData, UI.UIContentPanels.Inventory);
+            _inventoryCurrentData.inventoryItemList.Add(itemController.ItemData);
+            itemControllers.Add(itemController);
+        }
+
+        public List<ItemController> GetInventoryItems() => itemControllers;
     }
 }
--- Assets/Scripts/Inventory/InventoryService.cs
+++ Assets/Scripts/Inventory/InventoryService.cs
@@ -1,7 +1,23 @@
+using ServiceLocator.Item;
+
 namespace ServiceLocator.Inventory
 {
     public class InventoryService
     {
+        private InventoryController inventoryController;
+        public InventoryService(InventoryScriptableObject inventoryCurrentData)
+        {
+            inventoryController = new InventoryController(inventoryCurrentData);
 
+        }
+        ~InventoryService() { }
+
+        public void Initialize(InventoryScriptableObject inventoryInitialData, ItemService itemService)
+        {
+            foreach (var itemData in inventoryInitialData.inventoryItemList)
+            {
+                inventoryController.AddNewItemInInventory(itemData, itemService);
+            }
+        }
     }
 }
