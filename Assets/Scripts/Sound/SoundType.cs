081a716d71a6f0d7e6a7c394e3686fdeaf4aa3c0
Deependra-Das <iamdeep75@gmail.com>
2025-02-09 08:46:17 +0530

    Added Audio Sources & Scriptable Object for Audio List

---
+++ Assets/Scripts/Sound/SoundType.cs
@@ -0,0 +1,12 @@
+namespace ServiceLocator.Sound
+{
+    public enum SoundType
+    {
+        BackgroundMusic,
+        ButtonClick,
+        ItemClick,
+        CurrencyGain,
+        CurrencyLoss,
+        GatherResources,
+    }
+}
